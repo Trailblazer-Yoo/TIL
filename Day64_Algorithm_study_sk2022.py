62ca8cbd71c902b954d8fb2cc40062d8bbf5adf9
Trailblazer-Yoo <yousun9591@naver.com>
2022-03-16 23:13:56 +0900

    Day64

---
+++ Day64_Algorithm_study_sk2022.py
@@ -0,0 +1,29 @@
+def solution(money, costs):
+    costs_dict = {}
+    efficient = []
+    unit = [1, 5, 10, 50, 100, 500]
+    for key, value in zip(unit, costs):
+        costs_dict[key] = value
+        efficient.append((key - value) / key)
+    
+    efficient_min = list(reversed(sorted(efficient)))
+    idx_list = [unit[efficient.index(idx)] for idx in efficient_min]
+
+    money_iter = money
+    quantity_list = []
+    for efficient in idx_list:
+        dup = 0
+        while True:
+            money_iter -= efficient
+            if money_iter < 0:
+                break 
+            dup += 1
+        money_iter = money_iter + efficient
+        quantity_list.append(dup)
+
+    
+    values = [costs_dict[key] * quantity_list[i]  for i, key in enumerate(idx_list)]
+    
+    return sum(values)
+
+print(solution(1999,[2, 11, 20, 100, 200, 600]))

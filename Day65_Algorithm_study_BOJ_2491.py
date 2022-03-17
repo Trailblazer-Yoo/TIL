b8dfe49a8d4b2ce18482783e9220cd3c09d67ae3
Trailblazer-Yoo <yousun9591@naver.com>
2022-03-17 23:43:32 +0900

    Day65

---
+++ Day65_Algorithm_study_BOJ_2491.py
@@ -0,0 +1,17 @@
+n = int(input())
+num = [int(i) for i in input().split()]
+
+ans1 = [1]*n
+ans2 = [1]*n
+print(ans1)
+print(ans2)
+
+for i in range(1,n):
+    if num[i-1] <= num[i]:
+        ans1[i] = max(ans1[i], ans1[i-1] + 1)
+        print(ans1)
+    if num[i] <= num[i-1]:
+        ans2[i] = max(ans2[i], ans2[i-2] + 1)
+        print(ans2)
+        
+print(max(max(ans1), max(ans2)))
